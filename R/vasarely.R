9a21985e00546e3c8b37bcbc52a084b5f2ff7e0d
abanach <alexandra.banach@student.uni-luebeck.de>
2018-05-01 14:49:31 +0200

    calculation for testdata implemented: a priori probability, expected and real probability

--- R/vasarely.R
+++ R/vasarely.R
@@ -7,31 +7,41 @@
 #'
 #' @examples
 
-## do not forget: rm(list=ls())
-## now just trying something that works with test data :)
+### do not forget: rm(list=ls())
 
-# load our testdata
+### now just trying something that works with test data :)
+
+## load our testdata
 testdata <- as.data.frame(read.csv("testdata"))
+
+## compute a priori probability
+# get number of each allel in our two allels
 num_allel <- t(table(testdata$allel1) + table(testdata$allel2))
-names_colums <- colnames(num_allel)
-
-# calculate a priori probability
+# get a total number of all allels, -1 because first column is the id of our data
 num_all_allels <- (ncol(testdata) - 1) * nrow(testdata)
+# compute
 a_priori_prob <- as.vector(num_allel / num_all_allels)
+a_priori_prob
 
-# calculate expected probability
+## compute expected probability
 expected_probability <- as.data.frame(a_priori_prob %*% t(a_priori_prob))
-colnames(expected_probability) <- names_colums
-rownames(expected_probability) <- names_colums
+colnames(expected_probability) <- colnames(num_allel)
+rownames(expected_probability) <- colnames(num_allel)
+expected_probability
 
 
-# calculate real probability
+## compute real probability
+# maybe we need variable testdata later..
 testdata2 <- testdata
+# new column with allel combination
 testdata2$allel_combination <- paste(testdata$allel1,testdata$allel2)
+# delete both colums with redundant data
 testdata2$allel1 <- NULL
 testdata2$allel2 <- NULL
+#compute
 real_probability <- as.data.frame(table(testdata2$allel_combination)/nrow(testdata2))
 colnames(real_probability) <- c("allel_comb", "prob")
+real_probability
 
 
 
