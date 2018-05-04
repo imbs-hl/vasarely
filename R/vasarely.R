8f30d36e493ca76cb173fbb726ee4180ed6676ac
abanach <alexandra.banach@student.uni-luebeck.de>
2018-05-04 18:36:35 +0200

    vasarely function implemented

--- R/vasarely.R
+++ R/vasarely.R
@@ -11,7 +11,7 @@
 
 
 vasarely <- function(dat){
-  # prepare data
+  # prepare data: save input in dataframe and name columns to work with
   dat <- dat
   colnames(dat) <- c("allel1", "allel2")
 
@@ -19,43 +19,34 @@ vasarely <- function(dat){
   # get number of each allel in our two allels
   num_allel <- t(table(dat$allel1) + table(dat$allel2))
   # get a total number of all allels
-  num_all_allels <- ncol(dat) * nrow(testdata)
+  num_all_allels <- ncol(dat) * nrow(dat)
   # compute
   a_priori_prob <- as.vector(num_allel / num_all_allels)
 
 
   ## compute expected probability
-  expected_probability <- as.data.frame(a_priori_prob %*% t(a_priori_prob))
-  colnames(expected_probability) <- colnames(num_allel)
-  rownames(expected_probability) <- colnames(num_allel)
+  prob_ex <- as.vector(t(as.data.frame(a_priori_prob %*% t(a_priori_prob))))
 
 
   ## compute real probability
   # new column with allel combination
-  dat$allel_combination <- paste(testdata$allel1,testdata$allel2)
+  dat$allel_combination <- paste(dat$allel1,dat$allel2)
   #compute
   real_probability <- as.data.frame(table(dat$allel_combination)/nrow(dat))
   colnames(real_probability) <- c("allel_comb", "real_prob")
+  # vectors needed for plottig later
+  prob_real <- real_probability$real_prob
+  allel1 <- substring(real_probability$allel_comb,1,1)
+  allel2 <- substring(real_probability$allel_comb,2)
 
-  ## put real probability and expected probability together into a dataframe
-  probabilities <- real_probability
-  probabilities$expected_prob <- as.vector(t(expected_probability))
-  probabilities$allel1 <- substring(probabilities$allel_comb,1,1)
-  probabilities$allel2 <- substring(probabilities$allel_comb,2)
 
-  ## prepare chart
+  ## create plot for expected and real probability with geom_reaster and geom_dotplot
   library("ggplot2")
-  prob_ex <- probabilities$expected_prob
-  prob_real <- probabilities$real_prob
-  allel1 <- probabilities$allel1
-  allel2 <- probabilities$allel2
-
-  # create plot for expected and real probability with geom_reaster and geom_dotplot
-  print(ggplot(probabilities,aes(allel1, allel2)) +
+  print(ggplot(real_probability,aes(allel1, allel2)) +
           geom_raster(aes(fill=prob_ex), hjust = 0.5, vjust = 0.5, interpolate = FALSE) +
           geom_dotplot(aes(fill=prob_real), binwidth = 0.8, binaxis = "y", stackdir='center', color = 0.01))
 }
 
 # load second testdata (without column X) and test plot function
-data2 <- testdata <- as.data.frame(read.csv("testdata2"))
-vasarely(data2)
+testdata2 <- as.data.frame(read.csv("testdata2"))
+vasarely(testdata2)
